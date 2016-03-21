618d36ecce5d27b336783fe7de81b38a42e11c2f
Ben Hawker <ben@hawker.cc>
2016-03-20 18:53:11 -0700

    Tweak Helper

--- templates/helpers/isactive.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(section, subsection) {
-    for (var key in section.subsections){
-        if (section.subsections[key] == subsection) return true;
-    }
-    return false;
-};
