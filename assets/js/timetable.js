2d8df9dd07a0781b6170771b04831ec1a6ac27f8
Ben Hawker <ben@hawker.cc>
2016-03-27 00:35:52 -0700

    Fix popover behaviour

--- assets/js/timetable.js
+++ assets/js/timetable.js
@@ -1,11 +1,34 @@
 $(function(){
-    $(".event").each(function(i,e){
+    var events = $(".event");
+    events.each(function(i, e){
+        // Initialize Popovers
         var event = $(e);
         event.popover({
             container: '.mypage',
             html: true,
-            trigger: 'click',
+            trigger: 'manual',
             content: event.find(".event-popover").html()
         });
+
+        event.unbind().click(function(e){
+            if(event.hasClass('active')){
+                event.removeClass('active');
+                event.popover('hide');
+            } else {
+                events.each(function(i,e){
+                    var otherEvent = $(e);
+                    if(otherEvent.hasClass('active')){
+                        otherEvent.removeClass('active');
+                        otherEvent.popover('hide');
+                    }
+                });
+                event.addClass('active');
+                event.popover('show');
+            }
+        });
     });
+
+    function showPopover(){
+
+    }
 });
