6bd80438b9ac06651b251e592dc75762ed3c45ce
Ben Hawker <ben@hawker.cc>
2016-04-01 12:41:04 -0700

    Set up permalinks for Semester switching

--- assets/js/myuvic.js
+++ assets/js/myuvic.js
@@ -1,10 +1,36 @@
 $(function(){
-    $('[data-toggle="semester"]').each(function(){
-        var $switch = $(this);
-        if ($switch.is('select')) $switch.change(function(){
-            $('.semester.active').removeClass('active');
-            $(this.value).addClass('active');
+    $(document).ready(function(){
+        //Read the Hash
+        if(window.location.hash) {
+            var id = window.location.hash;
+            if ($('.semester' + id)){
+                SetSemester(id);
+            }
+        }
+    });
+
+    $('select[data-toggle="semester"]').each(function(){
+        $(this).change(function(){
+            SetSemester(this.value);
         });
     });
+
+    function SetSemester(id){
+        var $semester = $('.semester' + id);
+        if (!$semester) throw Error("No semesters found on page, you should remove the semester switch.",id);
+
+        //Hide other Semester
+        $('.semester.active').removeClass('active');
+
+        // Show the Current Semester
+        $semester.addClass('active');
+
+        // Set the Hash
+        if(history.pushState) history.pushState(null, null, id);
+        else location.hash = id;
+
+        //Set the selects
+        $('select[data-toggle="semester"]').val(id);
+    }
 
 });
